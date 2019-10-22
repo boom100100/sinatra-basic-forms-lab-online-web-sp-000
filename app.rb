c0428bfacd38d4555ee379366039c566a34d91f9
Bernadette Davis <bsd231@nyu.edu>
2019-10-22 19:24:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,17 +10,6 @@ class App < Sinatra::Base
   get '/new' do
     erb :create_puppy
   end
-=begin
-  post '/new' do
-    @myPuppy = Puppy.new(params[:name], params[:breed], params[:months_old])
-    @name = params[:name]
-    @breed = params[:breed]
-    @age = params[:months_old]
-
-    erb :display_puppy
-  end
-=end
-
 
   post '/puppy' do
 
@@ -31,10 +20,5 @@ class App < Sinatra::Base
 
     erb :display_puppy
   end
-=begin
-  get '/puppy' do
-    erb :display_puppy
-  end
 
-=end
 end
